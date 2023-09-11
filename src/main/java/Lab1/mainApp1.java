bc55bd5ad712033bf293aad8656eb20cdc720bf1
Jason William Nursalim <jwnursalim@gmail.com>
2023-09-11 18:00:00 +0800

    Add 1 Line - "Program Ended"

--- src/main/java/Lab1/mainApp1.java
+++ src/main/java/Lab1/mainApp1.java
@@ -8,5 +8,6 @@ public class mainApp1 {
         System.out.println("Program Started ...");
         System.out.println(b+" to power "+n+" = "+ myLibrary.Power(b,n));
         System.out.println(n+"! = "+ myLibrary.factorial(n));
+        System.out.println("Program Ended ...");
     }
 }
